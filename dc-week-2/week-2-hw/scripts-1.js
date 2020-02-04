4dde23a769506d929fa38c59eeffe20b90643687
kees911 <49771148+kees911@users.noreply.github.com>
2020-02-04 03:51:18 -0500

    Add files via upload

---
+++ dc-week-2/week-2-hw/scripts-1.js
@@ -0,0 +1,50 @@
+/*sources:
+Simple Draw (mobile) https://p5js.org/examples/mobile-simple-draw.html
+^This is the main one I used
+Drawing: https://p5js.org/examples/hello-p5-drawing.html
+Lines: https://p5js.org/examples/drawing-continous-lines.html
+Pulses https://p5js.org/examples/drawing-pulses.html
+*/
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  strokeWeight(5);
+  stroke (rOutput.innerHTML, gOutput.innerHTML, bOutput.innerHTML);
+}
+/*I don't know how to get this to respond to the actual slider.
+I think it has something to do with render() or a delay/refresh,
+but I don't know how I would implement that without refreshing the entire canvas
+as the stroke color function is part of the setup function*/
+
+
+function touchMoved() {
+  line(mouseX, mouseY, pmouseX, pmouseY);
+  return false;
+}
+
+var rSlider = document.getElementById("red");
+var rOutput = document.getElementById("rValue");
+rOutput.innerHTML = rSlider.value;
+
+rSlider.oninput = function() {
+  rOutput.innerHTML = this.value;
+}
+
+var gSlider = document.getElementById("green");
+var gOutput = document.getElementById("gValue");
+gOutput.innerHTML = gSlider.value;
+
+gSlider.oninput = function() {
+  gOutput.innerHTML = this.value;
+}
+
+var bSlider = document.getElementById("blue");
+var bOutput = document.getElementById("bValue");
+bOutput.innerHTML = bSlider.value;
+
+bSlider.oninput = function() {
+  bOutput.innerHTML = this.value;
+}
+
+var rColor = "rSlider.value"
+var gColor = "gSlider.value"
+var bColor = "bSlider.value"
---
+++ dc-week-2/week-2-hw/scripts-2.js
@@ -0,0 +1,516 @@
+//0th row
+
+function fillSquare0() {
+  var element = document.getElementById("pixelButton0");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare1() {
+  var element = document.getElementById("pixelButton1");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare2() {
+  var element = document.getElementById("pixelButton2");
+  element.classList.toggle("shaded");
+}
+
+//made a million of these because I don't know how to apply a function to just one instance of a class/id
+
+function fillSquare3() {
+  var element = document.getElementById("pixelButton3");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare4() {
+  var element = document.getElementById("pixelButton4");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare5() {
+  var element = document.getElementById("pixelButton5");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare6() {
+  var element = document.getElementById("pixelButton6");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare7() {
+  var element = document.getElementById("pixelButton7");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare8() {
+  var element = document.getElementById("pixelButton8");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare9() {
+  var element = document.getElementById("pixelButton9");
+  element.classList.toggle("shaded");
+}
+
+//first row
+function fillSquare10() {
+  var element = document.getElementById("pixelButton10");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare11() {
+  var element = document.getElementById("pixelButton11");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare12() {
+  var element = document.getElementById("pixelButton12");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare13() {
+  var element = document.getElementById("pixelButton13");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare14() {
+  var element = document.getElementById("pixelButton14");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare15() {
+  var element = document.getElementById("pixelButton15");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare16() {
+  var element = document.getElementById("pixelButton16");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare17() {
+  var element = document.getElementById("pixelButton17");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare18() {
+  var element = document.getElementById("pixelButton18");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare19() {
+  var element = document.getElementById("pixelButton19");
+  element.classList.toggle("shaded");
+}
+//second row
+
+function fillSquare20() {
+  var element = document.getElementById("pixelButton20");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare21() {
+  var element = document.getElementById("pixelButton21");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare22() {
+  var element = document.getElementById("pixelButton22");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare23() {
+  var element = document.getElementById("pixelButton23");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare24() {
+  var element = document.getElementById("pixelButton24");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare25() {
+  var element = document.getElementById("pixelButton25");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare26() {
+  var element = document.getElementById("pixelButton26");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare27() {
+  var element = document.getElementById("pixelButton27");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare28() {
+  var element = document.getElementById("pixelButton28");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare29() {
+  var element = document.getElementById("pixelButton29");
+  element.classList.toggle("shaded");
+}
+
+//third row
+
+function fillSquare30() {
+  var element = document.getElementById("pixelButton30");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare31() {
+  var element = document.getElementById("pixelButton31");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare32() {
+  var element = document.getElementById("pixelButton32");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare33() {
+  var element = document.getElementById("pixelButton33");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare34() {
+  var element = document.getElementById("pixelButton34");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare35() {
+  var element = document.getElementById("pixelButton35");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare36() {
+  var element = document.getElementById("pixelButton36");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare37() {
+  var element = document.getElementById("pixelButton37");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare38() {
+  var element = document.getElementById("pixelButton38");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare39() {
+  var element = document.getElementById("pixelButton39");
+  element.classList.toggle("shaded");
+}
+
+//fourth row
+
+function fillSquare40() {
+  var element = document.getElementById("pixelButton40");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare41() {
+  var element = document.getElementById("pixelButton41");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare42() {
+  var element = document.getElementById("pixelButton42");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare43() {
+  var element = document.getElementById("pixelButton43");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare44() {
+  var element = document.getElementById("pixelButton44");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare45() {
+  var element = document.getElementById("pixelButton45");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare46() {
+  var element = document.getElementById("pixelButton46");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare47() {
+  var element = document.getElementById("pixelButton47");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare48() {
+  var element = document.getElementById("pixelButton48");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare49() {
+  var element = document.getElementById("pixelButton49");
+  element.classList.toggle("shaded");
+}
+
+//fifth row
+
+function fillSquare50() {
+  var element = document.getElementById("pixelButton50");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare51() {
+  var element = document.getElementById("pixelButton51");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare52() {
+  var element = document.getElementById("pixelButton52");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare53() {
+  var element = document.getElementById("pixelButton53");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare54() {
+  var element = document.getElementById("pixelButton54");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare55() {
+  var element = document.getElementById("pixelButton55");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare56() {
+  var element = document.getElementById("pixelButton56");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare57() {
+  var element = document.getElementById("pixelButton57");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare58() {
+  var element = document.getElementById("pixelButton58");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare59() {
+  var element = document.getElementById("pixelButton59");
+  element.classList.toggle("shaded");
+}
+//sixth row
+
+function fillSquare60() {
+  var element = document.getElementById("pixelButton60");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare61() {
+  var element = document.getElementById("pixelButton61");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare62() {
+  var element = document.getElementById("pixelButton62");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare63() {
+  var element = document.getElementById("pixelButton63");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare64() {
+  var element = document.getElementById("pixelButton64");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare65() {
+  var element = document.getElementById("pixelButton65");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare66() {
+  var element = document.getElementById("pixelButton66");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare67() {
+  var element = document.getElementById("pixelButton67");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare68() {
+  var element = document.getElementById("pixelButton68");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare69() {
+  var element = document.getElementById("pixelButton69");
+  element.classList.toggle("shaded");
+}
+//seventh row
+
+function fillSquare70() {
+  var element = document.getElementById("pixelButton70");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare71() {
+  var element = document.getElementById("pixelButton71");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare72() {
+  var element = document.getElementById("pixelButton72");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare73() {
+  var element = document.getElementById("pixelButton73");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare74() {
+  var element = document.getElementById("pixelButton74");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare75() {
+  var element = document.getElementById("pixelButton75");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare76() {
+  var element = document.getElementById("pixelButton76");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare77() {
+  var element = document.getElementById("pixelButton77");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare78() {
+  var element = document.getElementById("pixelButton78");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare79() {
+  var element = document.getElementById("pixelButton79");
+  element.classList.toggle("shaded");
+}
+//eighth row
+
+function fillSquare80() {
+  var element = document.getElementById("pixelButton80");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare81() {
+  var element = document.getElementById("pixelButton81");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare82() {
+  var element = document.getElementById("pixelButton82");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare83() {
+  var element = document.getElementById("pixelButton83");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare84() {
+  var element = document.getElementById("pixelButton84");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare85() {
+  var element = document.getElementById("pixelButton85");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare86() {
+  var element = document.getElementById("pixelButton86");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare87() {
+  var element = document.getElementById("pixelButton87");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare88() {
+  var element = document.getElementById("pixelButton88");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare89() {
+  var element = document.getElementById("pixelButton89");
+  element.classList.toggle("shaded");
+}
+
+//ninth row
+
+function fillSquare90() {
+  var element = document.getElementById("pixelButton90");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare91() {
+  var element = document.getElementById("pixelButton91");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare92() {
+  var element = document.getElementById("pixelButton92");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare93() {
+  var element = document.getElementById("pixelButton93");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare94() {
+  var element = document.getElementById("pixelButton94");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare95() {
+  var element = document.getElementById("pixelButton95");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare96() {
+  var element = document.getElementById("pixelButton96");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare97() {
+  var element = document.getElementById("pixelButton97");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare98() {
+  var element = document.getElementById("pixelButton98");
+  element.classList.toggle("shaded");
+}
+
+function fillSquare99() {
+  var element = document.getElementById("pixelButton99");
+  element.classList.toggle("shaded");
+}
