ec62ddeda7ac2589b5aa231ce9df33a17c85e9cc
kees911 <49771148+kees911@users.noreply.github.com>
2020-02-18 09:35:25 -0500

    added comments to js

--- dc-week-4/week-4-hw/scripts-noclick.js
+++ dc-week-4/week-4-hw/scripts-noclick.js
@@ -1,7 +1,11 @@
+//one line per second, canvas is however large the window is
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(1)
 }
+
+//referred to https://p5js.org/reference/#/p5/pmouseX
 
 function draw() {
 line(mouseX, mouseY, pmouseX, pmouseY);
